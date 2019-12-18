d33935140553f623f2afc718f581e817d441f0d3
kelvin <ziyoustep@gmail.com>
2019-12-18 22:18:18 +0800

    Replace csapp rio with nio

--- src/pd_http_server.cpp
+++ src/pd_http_server.cpp
@@ -10,31 +10,35 @@
 #include "pd_http_server.h"
 
 #define MAXLINE 3000
+
 std::string msg = "Hello from server";
 
 int main(int argc, char const *argv[]){
-    int server_fd;
-    if((server_fd = pd_listen("8008")) < 0){
+    SocketChannel sockchan;
+    int server_fd = sockchan.listen(SocketAddress("localhost", SERVER_PORT));
+    if(server_fd < 0){
         perror("Bind to port 8008 failed");
     }
+    std::cout << "Socket status: " << sockchan.get_socket().get_status() << std::endl;
+    std::cout << "local addr: " << sockchan.get_socket().get_local_addr().to_string() << std::endl;
+
 
     while(1){
         printf("\nWaiting for new connection\n");
 
-        int cnxxfd;
-        sockaddr_in clientaddr;
-        int clientlen = sizeof(clientaddr);
-        if ((cnxxfd = accept(server_fd, (struct sockaddr *)&clientaddr, (socklen_t*)&clientlen)) < 0){
-            perror("In accept failed");
-            exit(EXIT_FAILURE);
-        }
-        
-        char buffer[MAXLINE] = {0};
-        long valread = read( cnxxfd , buffer, MAXLINE);
-        std::cout << buffer << std::endl;
-        rio_writen(cnxxfd , (void *)msg.c_str(), msg.size());
-        std::cout << "Hello message sent" << std::endl;
-        close(cnxxfd);
+        SocketChannel accChan = std::move(sockchan.accept());
+        ByteBuffer buffer(BUFFSIZE);
+        buffer.clear();
+        accChan.read(buffer);
+        buffer.flip();
+        std::cout << buffer.to_string() << std::endl;
+
+        buffer.clear();
+        buffer.put((Byte*)msg.c_str(), 0, msg.size());
+        buffer.flip();
+        accChan.write(buffer);
+
+        accChan.close();
     }
     return 0;
 }
